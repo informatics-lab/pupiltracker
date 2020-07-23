6e1cd6647eb07664f6aeaa96126255bc28263035
Niall Robinson <niall.h.robinson@gmail.com>
2020-07-23 12:57:28 +0100

    now removes bounding box on image generation

--- analysis/generate.py
+++ analysis/generate.py
@@ -7,8 +7,9 @@ def generate_image(file_name):
     d = iris.load_cube(file_name)
     iplt.pcolormesh(d)
     plt.gca().coastlines()
+    plt.gca().margins(x=0, y=0)
     plt.gca().set_axis_off()
-    plt.savefig("./data.png", fgisize=(), bbox_inches="tight", dpi=1000)
+    plt.savefig("./data.png", bbox_inches="tight", dpi=500, pad_inches=0)
 
 if __name__ == '__main__':
-    generate_image("./global_daily_pmsl_mean_20200509.nc")
+    generate_image("global_daily_pmsl_mean_20200509.nc")
